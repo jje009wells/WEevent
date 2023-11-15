9ff4571482dd74434ad310ff213ad77c905935ef
Yannis <yz101@cs.wellesley.edu>
2023-11-14 21:31:08 -0500

    create a form for inputting all required event information. Optional inputs for uploading spam and selecting tags.

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from flask import (Flask, render_template, make_response, url_for, request,
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-# one or the other of these. Defaults to MySQL (PyMySQL)
-# change comment characters to switch to SQLite
-
 import cs304dbi as dbi
-# import cs304dbi_sqlite3 as dbi
 
 import random
 
@@ -24,6 +20,78 @@ app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 @app.route('/')
 def index():
     return render_template('main.html',title='Hello')
+
+@app.route('/create_event/', methods = ['GET', 'POST'])
+def create_event():
+    
+    if request.method == "GET":
+        return render_template("create_event.html", method="POST")
+
+    else:
+        event_name = request.form.get("event-name")
+        event_type = request.form.get("event-type")
+        short_description = request.form.get("short-description")
+        start_time = request.form.get("start-time")
+        end_time = request.form.get("end-time")
+        event_location = request.form.get("event-location")
+        rsvp = request.form.get("rsvp")
+        event_tag = request.form.get("event-tag")
+        full_description= request.form.get("full-description")
+        contact_email = request.form.get("contact-email")
+        spam = request.form.get("spam")
+
+        flag = False
+
+        # if missing event name 
+        if event_name == "":
+            flash("Please enter event name")
+            flag = True
+
+        # if missing event type 
+        if event_type == "":
+            flash("Please enter event type")
+            flag = True
+        
+        # if missing short description 
+        if short_description == "":
+            flash("Please enter short description")
+            flag = True
+
+        # if missing start time
+        if start_time == "":
+            flash("Please enter start time")
+            flag = True
+
+        # if missing end time
+        if end_time == "":
+            flash("Please enter end time")
+            flag = True
+
+        # if missing event location 
+        if event_location == "":
+            flash("Please enter event location")
+            flag = True
+        
+        # if missing rsvp information 
+        if rsvp == "":
+            flash("Please enter rsvp information")
+            flag = True
+
+        # if missing full_description 
+        if full_description == "":
+            flash("Please enter full description")
+            flag = True
+
+        # if missing contact email 
+        if contact_email == "":
+            flash("Please enter contact email")
+            flag = True
+
+        # if user enter all required inputs
+        if flag == True:
+            return redirect(url_for("create_event"))
+
+
 
 # You will probably not need the routes below, but they are here
 # just in case. Please delete them if you are not using them
@@ -75,7 +143,7 @@ if __name__ == '__main__':
     else:
         port = os.getuid()
     # set this local variable to 'wmdb' or your personal or team db
-    db_to_use = 'put_database_name_here_db' 
+    db_to_use = 'weevent_db' 
     print('will connect to {}'.format(db_to_use))
     dbi.conf(db_to_use)
     app.debug = True
